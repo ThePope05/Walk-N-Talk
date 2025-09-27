6ecaddd49e52abd69f6996536374ba882a84f460
ThePope05 <swobben05@gmail.com>
2025-09-27 22:38:31 +0200

    Added back button to question pages

--- resources/views/questions.blade.php
+++ resources/views/questions.blade.php
@@ -1,6 +1,6 @@
 <x-base-page>
     <div id="question-list">
-
+        <a class="rounded-2xl bg-[#519F66] text-[#F8F6EF] text-center mt-4 text-2xl py-2 px-14 absolute left-4" href="{{ route('ice-breakers') }}">Terug</a>
         <h1 class="question-title">{{ ucfirst($category) }} vragen</h1>
 
         <ul>
